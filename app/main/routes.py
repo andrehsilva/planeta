f9335a99ad80a32eab00735a21ab9eb095720f73
andrehsilva <andrehrsilva@gmail.com>
2025-10-21 08:06:27 -0300

    ajuste save_picture

--- app/main/routes.py
+++ app/main/routes.py
@@ -136,7 +136,11 @@ def save_picture(form_picture, subfolder='uploads'):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(current_app.root_path, 'static', subfolder, picture_fn)
+
+    # usa o caminho configurado no UPLOAD_FOLDER (volume real)
+    upload_folder = current_app.config['UPLOAD_FOLDER']
+    picture_path = os.path.join(upload_folder, picture_fn)
+
     form_picture.save(picture_path)
     return picture_fn
 
